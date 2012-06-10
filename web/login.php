d1f9f138e8ab7a5fe65d58a635f47ce662dfc9c5
Matthias Lee <matthias.a.lee@gmail.com>
2012-06-09 22:19:10 -0400

    pushing basic web stuff

---
+++ web/login.php
@@ -0,0 +1,74 @@
+<html>
+
+<!-- ********* STYLESHEET LINK ********* -->
+	
+	<head>
+
+		<LINK REL=StyleSheet HREF="styles.css" TYPE="text/css" MEDIA=screen>
+		<title> The Node: Door </title> 
+	
+	</head>
+
+
+<!-- ********* UNIVERSAL CONTENT ********* -->
+	
+	
+	<body>
+					
+		<div id="centerDiv"> 
+		
+				
+			<div id="logo">
+				<img src="media/logo.png" border="0" alt="//media"></a>					
+			</div>
+				
+				
+			<div id="webcambox">
+				<div id="webcam">
+					<img src="media/cat.png" border="0" alt="//media"></a>				
+				</div>								
+			</div>
+				
+				
+			<div id="loginbox">
+				<div id="logintext">
+					<h1> Login </h1>	
+				</div>	
+			</div>
+				
+				
+			<div id="unlockbox">
+				<div id="unlocktext">
+					<h1> Unlock </h1>	
+				</div>	
+			</div>				
+				
+	
+<!-- ********* INFO CONTENT ********* -->
+
+				
+			<div id="info">
+				<h3> infoinfoinfo! </h3>			
+			</div>
+				
+				
+				<div id="infotext">
+					<h2> Current Information </h2>
+				</div>
+					
+						
+			<div id="editbutton">
+				<div id="editbuttontext">	
+					<h2> edit current information </h2>
+				</div>
+			</div>
+
+
+	</div>
+	</body>
+</html>
+
+
+				
+
+
